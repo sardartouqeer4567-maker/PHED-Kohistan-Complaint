b5804c0b4afc0489e29badc9a86c2ecefd961839
Touqeer Ahmed <sardartouqeer4567@gmail.com>
2026-08-17 00:27:22 +0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,16 @@
 plugins {
-    id("com.android.application") version "8.11.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.10" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
-    id("com.google.gms.google-services") version "4.4.3" apply false
+    id("com.android.application")
 }
-
 
 android {
     namespace = "com.phed.kohistan"
-    compileSdk = 36
+    compileSdk = 35
+
     defaultConfig {
         applicationId = "pk.gov.kp.phed.kohistan.complaints"
-        minSdk = 26
-        targetSdk = 36
-        versionCode = 10
-        versionName = "1.0.0"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
     }
-    buildFeatures { compose = true }
-}
-
-dependencies {
-    implementation(platform("com.google.firebase:firebase-bom:34.16.0"))
-
-    implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.firebase:firebase-firestore")
-    implementation("com.google.firebase:firebase-storage")
-    implementation("com.google.firebase:firebase-messaging")
-    implementation("com.google.firebase:firebase-appcheck-playintegrity")
-    implementation("com.google.firebase:firebase-crashlytics")
 }
